9e4d84a11294dc41100019b7eb244f8692ca5aec
Arthur Santos <arthurluiz.developer@gmail.com>
2025-05-01 17:45:03 +0100

    Projecto inicializado

---
+++ PlayerManager3/Player.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace PlayerManager3
+{
+    public class Player
+    {
+        public string Name { get; }
+        public int Score { get; set; }
+        
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - Score: {Score}";
+        }
+    }
+}
